3a45de8e59356d884d7d2abd565204cef78c80da
Hao <hao.streamnz@gmail.com>
2025-02-05 07:49:26 +0530

    week9 class activity about barchar and category in different color

--- week9/BarChar.py
+++ week9/BarChar.py
@@ -3,7 +3,10 @@ import matplotlib.pyplot as plt
 categories = ['A', 'B', 'C', 'D']
 values = [3, 7, 1, 8]
 
-plt.bar(categories, values, color='skyblue')
+# 为每个柱条设置不同的颜色
+colors = ['red', 'green', 'blue', 'orange']
+
+plt.bar(categories, values, color=colors)
 plt.xlabel('Categories')
 plt.ylabel('Values')
 plt.title('Bar Chart Example')
